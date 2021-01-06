9be1f60a744325e0ea1dc7c39c3435584279edb7
amiRai <amirai0396@gmail.com>
2021-01-07 02:55:33 +0530

    products in local browser storage

--- index.js
+++ index.js
@@ -52,12 +52,21 @@ return products
 }
     }
     // ------------local storage 
-    class Storage{}
+    class Storage{
+        static saveProducts(products){
+            localStorage.setItem("products",JSON.stringify(products))
+        }
+    }
 
     document.addEventListener('DOMContentLoaded', ()=>{
 const ui = new UI();
 const products = new Products();
 
 // --------get all products----------
-products.getProducts().then(products => ui.displayProducts(products))
+products.getProducts().then(products => {
+    ui.displayProducts(products);
+    Storage.saveProducts(products);
+}).then(()=>{
+
+})
     });
